b9f8f01a80f32834fde8bad1d927153102e24428
Zoewy007 <ttzoewy@gmail.com>
2023-02-17 12:49:58 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void)
+{
+	int alpha_low;
+	int alpha_high;
+	
+	alpha_low = 'a';
+	while (alpha_low <= 'z')
+	{
+		putchar(alpha_low);
+		alpha_low++;
+	}
+	alpha_high = 'A';
+
+	while (alpha_high <= 'Z')
+	{
+		putchar(alpha_high);
+		alpha_high++;
+	}
+	return (0);
+}
